17e4a8d58924930ebc03a6e833eed620089572ed
Bek <thedevbek@proton.me>
2023-08-05 03:40:42 -0500

    Update serving hander

--- src/js/controller.js
+++ src/js/controller.js
@@ -59,11 +59,19 @@ const constrolPagination = function (gotToPage) {
     paginationView.render(model.state.search);
 } 
 
+const controlServings = function (newServings) {
+  // update the recipe servings (in state)
+  model.updateServings(newServings);
+  // Update the recipe view
+  recipeView.render(model.state.recipe);
+}
 
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
   searchView.addHandlerSearch(controlSearchResults);
-  paginationView.addHanderCick(constrolPagination)
+  paginationView.addHanderCick(constrolPagination);
+  
 };
 
 init();
--- src/js/model.js
+++ src/js/model.js
@@ -70,5 +70,12 @@ export const getSearchResultsPage = function (page = state.search.page) {
 };
 
 
+export const updateServings = function (newServings) {
+    state.recipe.ingredients.forEach(ing => {
+        ing.quantity = (ing.quantity * newServings) / state.recipe.servings;
+            // newQt = oldQt * newServings / oldServings // 2 * 8 /4 =4
+    });
 
+    state.recipe.servings = newServings;
+}
 
