4df3c6a7c64100df8eaf52b42f5997df115bc6e7
Bek <thedevbek@proton.me>
2023-08-08 13:09:10 -0500

    finished tutorial

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,9 @@
 export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = 'a9b76f6c-4ed7-4b56-987d-1fda6e078417';
+export const KEY = '6c09f240-d48d-42c8-b40d-6de3793d9c98';
 export const MODAL_CLOSE_SEC = 2.5;
+
+
+
+
